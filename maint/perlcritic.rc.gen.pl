3df24c4eb13c4cf0efca146d3e623969c4bf53db
Kent Fredric <kentfredric@gmail.com>
2014-04-27 21:00:31 +1200

    Build results of 617b4f1 (on master)

--- maint/perlcritic.rc.gen.pl
+++ maint/perlcritic.rc.gen.pl
@@ -28,6 +28,7 @@ for my $wordlist (@stopwords) {
 
 #$bundle->remove_policy('ErrorHandling::RequireCarping');
 $bundle->remove_policy('NamingConventions::Capitalization');
+$bundle->remove_policy('NamingConventions::ProhibitMixedCaseSubs');
 
 my $inf = $bundle->actionlist->get_inflated;
 
