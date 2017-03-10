ec938ac60261b80f9f52e620b0afe291b7799846
Kent Fredric <kentfredric@gmail.com>
2017-03-11 03:48:46 +1300

    v1.000002
     - Bugfix: Avoid test failures with -Ddefault_inc_excludes_dot
    
     [Dependencies::Stats]
     - Dependencies changed since 1.000001, see misc/*.deps* for details
     - configure: +1 ↓1 (recommends: ↑1)
     - develop: +18 ↑4 -6 (recommends: +1, suggests: +2 -1)
     - runtime: ↓1 -1
     - test: +2 ↓1 -3 (recommends: ↑2 ↓1 -1)

--- lib/Dist/Zilla/Util/SimpleMunge.pm
+++ lib/Dist/Zilla/Util/SimpleMunge.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Util::SimpleMunge;
 
-our $VERSION = '1.000002';
+our $VERSION = '1.000003';
 
 # ABSTRACT: Make munging File::FromCode and File::InMemory easier.
 
